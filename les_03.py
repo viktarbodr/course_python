5801b1b8dfae5e7e9a6c2eb794eca7efabf15e2d
Viktar <viktar-barodka>
2020-12-20 14:59:23 +0300

    added file les_03

---
+++ les_03.py
@@ -0,0 +1,8 @@
+m = int(input('Введите Ваш вес (в килограммах) :  '))
+h = float(input('Введите Ваш рост (в метрах) :  '))
+bmi = m / h ** 2
+
+steps_1 = round(bmi) - 11
+steps_2 = 49 - round(bmi)
+print(f'Ваш индекс массы тела равен: {round(bmi)}\n')
+print('10' + '=' * (step_1) +'|'+ '=' * (step_2) + '50')
