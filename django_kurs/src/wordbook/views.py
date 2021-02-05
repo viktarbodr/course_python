48f9aa5540cf4fd2cc0d78502feef2bd680013c9
Viktar <viktar-barodka>
2021-02-05 16:42:37 +0300

    added templates,urls,,views

--- django_kurs/src/wordbook/views.py
+++ django_kurs/src/wordbook/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from wordbook.models import Author
+f_aut = Author.objects.first
 
-# Create your views here.
+def home_page(request):
+    context = {'f_aut': f_aut}
+    return render(request, template_name = 'home.html', context=context)
